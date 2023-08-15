6c20a4a3e2432a669528f8b5ce064d6de0633d60
RayaneKettana <rkettana@gmail.com>
2023-08-15 14:01:10 +0200

    Implemented the OfferingController to expose an endpoint for creating new offerings

--- src/main/java/com/jobjet/application/controllers/OfferingController.java
+++ src/main/java/com/jobjet/application/controllers/OfferingController.java
@@ -1,13 +1,27 @@
 package com.jobjet.application.controllers;
 
 import com.jobjet.domain.usecases.CreateOfferingUseCase;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.jobjet.domain.entities.Offering;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/services")
+@RequestMapping("/offerings")
 public class OfferingController {
 
+  private final CreateOfferingUseCase createOfferingUseCase;
+
   public OfferingController(CreateOfferingUseCase createOfferingUseCase) {
+    this.createOfferingUseCase = createOfferingUseCase;
+  }
+
+  @PostMapping
+  public ResponseEntity<Offering> createOffering(@RequestBody CreateOfferingUseCase.CreateOfferingInput input) {
+    try {
+      Offering newOffering = createOfferingUseCase.execute(input);
+      return ResponseEntity.status(201).body(newOffering);
+    } catch (Exception e) {
+      return ResponseEntity.badRequest().build();
+    }
   }
 }
