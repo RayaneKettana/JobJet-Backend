20557c323ac624d0e9951df0cfc1ced14ee7811e
RayaneKettana <rkettana@gmail.com>
2023-08-17 01:12:51 +0200

    Add test to ensure OfferingAlreadyExist exception is thrown

--- src/test/java/com/jobjet/domain/usecases/CreateOfferingUseCaseTest.java
+++ src/test/java/com/jobjet/domain/usecases/CreateOfferingUseCaseTest.java
@@ -2,6 +2,7 @@ package com.jobjet.domain.usecases;
 
 import com.jobjet.domain.entities.Offering;
 import com.jobjet.domain.repositories.IOfferingRepository;
+import com.jobjet.domain.usecases.exceptions.OfferingAlreadyExist;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -13,6 +14,8 @@ import com.jobjet.domain.usecases.inputs.CreateOfferingInput;
 
 import java.math.BigDecimal;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.*;
 
 class CreateOfferingUseCaseTest {
@@ -43,4 +46,21 @@ class CreateOfferingUseCaseTest {
         Assertions.assertThat(result).isNotNull();
         Assertions.assertThat(result.name()).isEqualTo("Test");
     }
+
+    @Test
+    @DisplayName("shouldThrowOfferingAlreadyExist")
+    public void shouldThrowOfferingAlreadyExist() {
+        // Arrange
+        CreateOfferingInput input = new CreateOfferingInput("Test", new BigDecimal(10), 1);
+        when(offeringRepository.exists(anyString())).thenReturn(true);
+
+        // Act & Assert
+        Exception exception = assertThrows(OfferingAlreadyExist.class, () -> {
+            createOfferingUseCase.execute(input);
+        });
+
+        // Optional: Further assertions on the exception if necessary
+        assertTrue(exception.getMessage().contains("L'offre spécifiée existe déjà."));
+    }
+
 }
