6b2b76f0894b833c71fa6d8060d7243500b1b81f
RayaneKettana <rkettana@gmail.com>
2023-08-16 20:41:50 +0200

    Add test for CreateOfferingUseCaseTest

--- src/test/java/com/jobjet/domain/usecases/CreateOfferingUseCaseTest.java
+++ src/test/java/com/jobjet/domain/usecases/CreateOfferingUseCaseTest.java
@@ -1,24 +1,46 @@
 package com.jobjet.domain.usecases;
 
+import com.jobjet.domain.entities.Offering;
+import com.jobjet.domain.repositories.IOfferingRepository;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import com.jobjet.domain.usecases.inputs.CreateOfferingInput;
+
+import java.math.BigDecimal;
+
+import static org.mockito.Mockito.*;
 
 class CreateOfferingUseCaseTest {
 
+    @InjectMocks
     private CreateOfferingUseCase createOfferingUseCase;
+
+    @Mock
+    private IOfferingRepository offeringRepository;
 
     @BeforeEach
     void setUp() {
-//        createOfferingUseCase = new CreateOfferingUseCase();
+        MockitoAnnotations.initMocks(this); // Initialise les mocks
     }
 
     @Test
-    @DisplayName("shouldAssertSomething")
-    public void shouldAssertSomething() {
-//        createOfferingUseCase.execute()
-        Assertions.assertThat(false).isTrue();
-    }
+    @DisplayName("shouldCreateOffering")
+    public void shouldCreateOffering() {
+        // Arrange
+        CreateOfferingInput input = new CreateOfferingInput("Test", new BigDecimal(10), 1);
+        Offering mockOffering = new Offering(null, "Test", new BigDecimal(10), 1);
+        when(offeringRepository.save(any(Offering.class))).thenReturn(mockOffering);
 
+        // Act
+        Offering result = createOfferingUseCase.execute(input);
+
+        // Assert
+        Assertions.assertThat(result).isNotNull();
+        Assertions.assertThat(result.name()).isEqualTo("Test");
+    }
 }
